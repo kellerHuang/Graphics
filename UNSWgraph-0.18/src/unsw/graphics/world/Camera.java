5b3a1fb89563bb62dfcbe3e13182c30030150139
Evan <gabokazu154@gmail.com>
2018-10-07 21:13:18 +1100

    some texture and light

--- UNSWgraph-0.18/src/unsw/graphics/world/Camera.java
+++ UNSWgraph-0.18/src/unsw/graphics/world/Camera.java
@@ -22,80 +22,80 @@ import unsw.graphics.geometry.Point3D;
  */
 public class Camera implements KeyListener {
 
-    private Point3D myPos;
-    private float myAngle;
-    private float myScale;
-    private Terrain terrain;
-    public Camera(Terrain terrain) {
-        myPos = new Point3D(0, 0,20);
-        myAngle = 0;
-        myScale = 1f;
-        this.terrain = terrain;
-    }
-    
-    public void draw(GL3 gl, CoordFrame3D frame) {
-        CoordFrame3D cameraFrame = frame.translate(myPos)
-                .rotateZ(myAngle)
-                .scale(myScale, myScale,myScale);
-    }
+	private Point3D myPos;
+	private float myAngle;
+	private float myScale;
+	private Terrain terrain;
+	public Camera(Terrain terrain) {
+		myPos = new Point3D(0, 0,20);
+		myAngle = 0;
+		myScale = 1f;
+		this.terrain = terrain;
+	}
 
-    /**
-     * Set the view transform
-     * 
-     * Note: this is the inverse of the model transform above
-     * 
-     * @param gl
-     */
-    public void setView(GL3 gl) {
-        CoordFrame3D viewFrame = CoordFrame3D.identity()
-                .scale(1/myScale, 1/myScale, 1/myScale)
-                .rotateY(myAngle)
-                .translate(-myPos.getX(), -myPos.getY(), -myPos.getZ());
-        Shader.setViewMatrix(gl, viewFrame.getMatrix());
-    }
+	public void draw(GL3 gl, CoordFrame3D frame) {
+		CoordFrame3D cameraFrame = frame.translate(myPos)
+				.rotateZ(myAngle)
+				.scale(myScale, myScale,myScale);
+	}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-    	float x;
-    	float y;
-        switch(e.getKeyCode()) {
-        case KeyEvent.VK_LEFT:
-        	System.out.println("left");
-        	myAngle -=30;
-        	break;
-        case KeyEvent.VK_RIGHT:
-           	System.out.println("right");
-        	myAngle += 30; 
-        	break;
-        case KeyEvent.VK_DOWN:
-        	System.out.println("down");
-        	x = (float)Math.sin(Math.toRadians(myAngle))/5;
-        	y = (float)Math.cos(Math.toRadians(myAngle))/5;
-        	myPos = myPos.translate(-x,0,y);
-        	myPos = myPos.translate(0, terrain.altitude(myPos.getX(), myPos.getZ())-myPos.getY(), 0);
-        	System.out.println(myAngle);
-        	System.out.println(terrain.altitude(myPos.getX(), myPos.getZ()));
-        	System.out.println(myPos.getX());
-        	System.out.println(myPos.getY());
-        	System.out.println(myPos.getZ());
-        	break;
-        case KeyEvent.VK_UP:
-        	System.out.println("up");
-        	x = (float)Math.sin(Math.toRadians(myAngle))/5;
-        	y = (float)Math.cos(Math.toRadians(myAngle))/5;
-        	myPos = myPos.translate(x,0,-y);
-        	myPos = myPos.translate(0, terrain.altitude(myPos.getX(), myPos.getZ())-myPos.getY(), 0);
-        	System.out.println(myAngle);
-        	System.out.println(terrain.altitude(myPos.getX(), myPos.getZ()));
-        	System.out.println(myPos.getX());
-        	System.out.println(myPos.getY());
-        	System.out.println(myPos.getZ());
-        	break;
-        }
+	/**
+	 * Set the view transform
+	 *
+	 * Note: this is the inverse of the model transform above
+	 *
+	 * @param gl
+	 */
+	public void setView(GL3 gl) {
+		CoordFrame3D viewFrame = CoordFrame3D.identity()
+				.scale(1/myScale, 1/myScale, 1/myScale)
+				.rotateY(myAngle)
+				.translate(-myPos.getX(), -myPos.getY(), -myPos.getZ());
+		Shader.setViewMatrix(gl, viewFrame.getMatrix());
+	}
 
-    }
+	@Override
+	public void keyPressed(KeyEvent e) {
+		float x;
+		float y;
+		switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				System.out.println("left");
+				myAngle -=30;
+				break;
+			case KeyEvent.VK_RIGHT:
+				System.out.println("right");
+				myAngle += 30;
+				break;
+			case KeyEvent.VK_DOWN:
+				System.out.println("down");
+				x = (float)Math.sin(Math.toRadians(myAngle))/5;
+				y = (float)Math.cos(Math.toRadians(myAngle))/5;
+				myPos = myPos.translate(-x,0,y);
+				myPos = myPos.translate(0, terrain.altitude(myPos.getX(), myPos.getZ())-myPos.getY(), 0);
+				System.out.println(myAngle);
+				System.out.println(terrain.altitude(myPos.getX(), myPos.getZ()));
+				System.out.println(myPos.getX());
+				System.out.println(myPos.getY());
+				System.out.println(myPos.getZ());
+				break;
+			case KeyEvent.VK_UP:
+				System.out.println("up");
+				x = (float)Math.sin(Math.toRadians(myAngle))/5;
+				y = (float)Math.cos(Math.toRadians(myAngle))/5;
+				myPos = myPos.translate(x,0,-y);
+				myPos = myPos.translate(0, terrain.altitude(myPos.getX(), myPos.getZ())-myPos.getY(), 0);
+				System.out.println(myAngle);
+				System.out.println(terrain.altitude(myPos.getX(), myPos.getZ()));
+				System.out.println(myPos.getX());
+				System.out.println(myPos.getY());
+				System.out.println(myPos.getZ());
+				break;
+		}
 
-    @Override
-    public void keyReleased(KeyEvent e) {}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
 
 }
--- UNSWgraph-0.18/src/unsw/graphics/world/World.java
+++ UNSWgraph-0.18/src/unsw/graphics/world/World.java
@@ -1,6 +1,5 @@
 package unsw.graphics.world;
 
-import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,7 +17,7 @@ import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.newt.event.KeyListener;
 
 /**
- * COMMENT: Comment Game 
+ * COMMENT: Comment Game
  *
  * @author malcolmr
  */
@@ -28,18 +27,18 @@ public class World extends Application3D{
     private Camera camera;
     private Point3D myPos;
     private float myAngle;
-    
+
     public World(Terrain terrain) {
-    	super("Assignment 2", 800, 600);
+        super("Assignment 2", 800, 600);
         this.terrain = terrain;
         camera = new Camera(terrain);
         myAngle = 0;
         myPos = new Point3D(0,0,0);
     }
-   
+
     /**
      * Load a level file and display it.
-     * 
+     *
      * @param args - The first argument is a level file in JSON format
      * @throws FileNotFoundException
      */
@@ -49,43 +48,33 @@ public class World extends Application3D{
         world.start();
     }
 
-	@Override
-	public void display(GL3 gl) {
-		super.display(gl);
-		camera.setView(gl);
-		terrain.drawTerrain(gl,CoordFrame3D.identity());
-	}
+    @Override
+    public void display(GL3 gl) {
+        super.display(gl);
+        camera.setView(gl);
+        terrain.terrainDisplay(gl,CoordFrame3D.identity());
+    }
 
-	@Override
-	public void destroy(GL3 gl) {
-		super.destroy(gl);
-		terrain.destroy(gl);
-		
-	}
+    @Override
+    public void destroy(GL3 gl) {
+        super.destroy(gl);
+        terrain.terrainDestroy(gl);
 
-	@Override
-	public void init(GL3 gl) {
-		super.init(gl);
+    }
+
+    @Override
+    public void init(GL3 gl) {
+        super.init(gl);
         getWindow().addKeyListener(camera);
-//	    Shader shader = new Shader(gl, "shaders/vertex_tex_phong.glsl", "shaders/fragment_tex_phong.glsl");
-//	    shader.use(gl);
-//	    // Test light
-//	    Shader.setPoint3D(gl, "lightPos", new Point3D(0, 0, 5));
-//	    Shader.setColor(gl, "lightIntensity", Color.WHITE);
-//	    Shader.setColor(gl, "ambientIntensity", new Color(0.3f, 0.3f, 0.3f));
-//	
-//	    Shader.setColor(gl, "ambientCoeff", Color.WHITE);
-//	    Shader.setColor(gl, "diffuseCoeff", new Color(0.5f, 0.5f, 0.5f));
-//	    Shader.setColor(gl, "specularCoeff", new Color(0.75f, 0.75f, 0.75f));
-//	    Shader.setFloat(gl, "phongExp", 16f);
-		terrain.init(gl);
-	}
+        terrain.terrainInit(gl);
+    }
 
-	@Override
-	public void reshape(GL3 gl, int width, int height) {
+    @Override
+    public void reshape(GL3 gl, int width, int height) {
         super.reshape(gl, width, height);
         Shader.setProjMatrix(gl, Matrix4.perspective(60, width/(float)height, 0.01f, 100));
-	}
-	
+        terrain.terrainReshape(gl, width, height);
+    }
+
 
 }
