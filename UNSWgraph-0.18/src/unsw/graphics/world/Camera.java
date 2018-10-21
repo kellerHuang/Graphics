8e039ed95fe94d48cc3bcd9676d13611e8cc7e3c
kellerHuang <keller.huang98@gmail.com>
2018-10-21 21:43:16 +1100

    walking

--- UNSWgraph-0.18/src/unsw/graphics/world/Camera.java
+++ UNSWgraph-0.18/src/unsw/graphics/world/Camera.java
@@ -1,6 +1,7 @@
 package unsw.graphics.world;
 
 
+import com.jogamp.graph.geom.Triangle;
 import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.newt.event.KeyListener;
 import com.jogamp.opengl.GL;
@@ -14,6 +15,8 @@ import unsw.graphics.geometry.TriangleMesh;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -30,12 +33,21 @@ public class Camera implements KeyListener {
 	private Terrain terrain;
 	private Texture texture;
 	private TriangleMesh model;
+	private TriangleMesh leg1;
+	private TriangleMesh leg2;
+	private float offset;
+	private boolean add;
+	private int walking;
 	public Camera(Terrain terrain) throws IOException {
 		myPos = new Point3D(0, 1,20);
 		myAngle = 0;
 		myScale = 1f;
-		model = new TriangleMesh("res/models/bunny.ply", true, true);
+		//model = new TriangleMesh("res/models/bunny.ply", true, true);
+        generateAvatar();
 		this.terrain = terrain;
+		offset = 0;
+		add = true;
+		walking = 0;
 	}
 
 	public void draw(GL3 gl, CoordFrame3D frame) {
@@ -45,7 +57,10 @@ public class Camera implements KeyListener {
 	}
 	public void init(GL3 gl) {
 		model.init(gl);
-		texture = new Texture(gl, "res/textures/rock.bmp", "bmp", false);
+		leg1.init(gl);
+		leg2.init(gl);
+
+		texture = new Texture(gl, "res/textures/kittens.jpg", "jpg", false);
 		Shader shader = new Shader(gl, "shaders/vertex_tex_phong.glsl", "shaders/fragment_tex_phong.glsl");
 		shader.use(gl);
 	}
@@ -53,7 +68,21 @@ public class Camera implements KeyListener {
 		Shader.setInt (gl, "tex", 0);
 		gl.glActiveTexture(GL.GL_TEXTURE0);
 		gl.glBindTexture(GL.GL_TEXTURE_2D, texture.getId());
-		Shader.setPenColor(gl, Color.GREEN);
+		Shader.setPenColor(gl, Color.WHITE);
+		if (walking > 0) {
+            if (add) {
+                offset += 3;
+            } else {
+                offset -= 3;
+            }
+            if (offset == 60) {
+                add = false;
+            }
+            if (offset == -60) {
+                add = true;
+            }
+        }
+        System.out.println(offset);
 //
 //		// Test light
 		Matrix4 rotate = Matrix4.rotationY(myAngle);
@@ -68,8 +97,21 @@ public class Camera implements KeyListener {
 		Shader.setColor(gl, "specularCoeff", new Color(0.75f, 0.75f, 0.75f));
 		Shader.setFloat(gl, "phongExp", 16f);
 
-		CoordFrame3D frame = CoordFrame3D.identity().translate(myPos).translate(0,-1,0).rotateY(-myAngle+180).scale(2,2,2);
-		model.draw (gl, frame);
+		CoordFrame3D frame = CoordFrame3D.identity().translate(myPos).translate(0,-0.8f,0).rotateY(-myAngle+180).scale(0.3f,0.3f,0.3f);
+        model.draw(gl,frame);
+//        frame = CoordFrame3D.identity().translate(myPos).translate(-2,-1,0).rotateY(-myAngle+180).scale(0.2f,0.2f,0.2f);
+        if(walking > 0) {
+            frame = CoordFrame3D.identity().translate(myPos).translate(0,0.2f,0).rotateY(-myAngle).scale(0.1f, 0.1f, 0.1f).translate(-3.5f, -9.5f, 0).rotateX(180).rotateX(offset);
+            leg1.draw(gl, frame);
+            frame = CoordFrame3D.identity().translate(myPos).translate(0,0.2f,0).rotateY(-myAngle).scale(0.1f, 0.1f, 0.1f).translate(-0.5f, -9.5f, 0).rotateX(180).rotateX(-offset);
+            leg2.draw(gl, frame);
+            walking--;
+        }else{
+            frame = CoordFrame3D.identity().translate(myPos).translate(0,0.2f,0).rotateY(-myAngle).scale(0.1f,0.1f,0.1f).translate(-3.5f,-9.5f,0).rotateX(180);
+            leg1.draw(gl,frame);
+            frame = CoordFrame3D.identity().translate(myPos).translate(0,0.2f,0).rotateY(-myAngle).scale(0.1f,0.1f,0.1f).translate(-0.5f,-9.5f,0).rotateX(180);
+            leg2.draw(gl,frame);
+        }
 	}
 	public Point3D getMyPos(){
 		return myPos;
@@ -91,11 +133,78 @@ public class Camera implements KeyListener {
 				.translate(-myPos.getX(), -myPos.getY(), -myPos.getZ());
 		Shader.setViewMatrix(gl, viewFrame.getMatrix());
 	}
+    public void generateAvatar(){
+        List<Point3D> vertices = new ArrayList<>();
+        List<Integer> indices = new ArrayList<>();
 
+        // Body
+        vertices.add(new Point3D(0,0,0));
+        vertices.add(new Point3D(0,2,0));
+        vertices.add(new Point3D(1,2,0));
+        vertices.add(new Point3D(1,0,0));
+        vertices.add(new Point3D(0,0,1));
+        vertices.add(new Point3D(0,2,1));
+        vertices.add(new Point3D(1,2,1));
+        vertices.add(new Point3D(1,0,1));
+
+        indices.add(3);
+        indices.add(0);
+        indices.add(1);
+
+        indices.add(3);
+        indices.add(1);
+        indices.add(2);
+
+        indices.add(6);
+        indices.add(3);
+        indices.add(2);
+
+        indices.add(7);
+        indices.add(3);
+        indices.add(6);
+
+        indices.add(4);
+        indices.add(7);
+        indices.add(6);
+
+        indices.add(5);
+        indices.add(4);
+        indices.add(6);
+
+        indices.add(1);
+        indices.add(0);
+        indices.add(5);
+
+        indices.add(0);
+        indices.add(4);
+        indices.add(1);
+
+        indices.add(2);
+        indices.add(1);
+        indices.add(5);
+
+        indices.add(2);
+        indices.add(5);
+        indices.add(6);
+
+        indices.add(3);
+        indices.add(4);
+        indices.add(0);
+
+        indices.add(7);
+        indices.add(4);
+        indices.add(3);
+
+
+        model = new TriangleMesh(vertices,indices,true);
+        leg1 = new TriangleMesh(vertices,indices,true);
+        leg2 = new TriangleMesh(vertices,indices,true);
+    }
 	@Override
 	public void keyPressed(KeyEvent e) {
 		float x;
 		float y;
+		walking = 30;
 		switch(e.getKeyCode()) {
 			case KeyEvent.VK_LEFT:
 				myAngle -=30;
